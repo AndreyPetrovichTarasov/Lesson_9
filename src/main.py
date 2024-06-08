c1b1b90496dc74061de1fb06aaf73c0fd93e2837
Andrey_Tarasov <lacryk@gmail.com>
2024-06-08 12:19:05 +0300

    filter russian names

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def clear_names(file_name: str) -> list:
     """ Функция для очистки имен от лишних символов """
     new_names_list = list()
@@ -13,8 +16,22 @@ def clear_names(file_name: str) -> list:
     return new_names_list
 
 
+def is_cyrilic(name_item: str) -> bool:
+    """ Проверка на русские символы """
+
+    return bool(re.search("[а-яА-Я]", name_item))
+
+def filter_russian_names(names_list: list) -> list:
+    """ Фильтрайия русских имен """
+    new_names_list = list()
+    for name_item in names_list:
+        if is_cyrilic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
+
 if __name__ == "__main__":
     c_names = clear_names("names.txt")
 
-    for i in c_names:
-        print(i)
+print(filter_russian_names(c_names))
+
